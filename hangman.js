af35920ce83aa871be5c62fc58be5aa249ed3e97
gergokutu <gergo.kutu@gmail.com>
2019-07-09 16:32:20 +0200

    wrong quess counter function

--- hangman.js
+++ hangman.js
@@ -1,6 +1,8 @@
 // Write a function that takes a word, and an array of guessed letters, and returns count of failed guesses.
 function wrongGuessCount(word, guesses) {
-    
+    return guesses
+        .filter(letter => word.indexOf(letter) < 0)
+        .length
   }
   
   console.log('wrong guesses: ', wrongGuessCount('hello', ['e', 'd', 'x', 'o']), 'should be:', 2)
